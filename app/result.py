547bd5208ce9d9cac2a47432114eb6c5086576c4
jankrukowski <jan.krukowski@tigerspike.com>
2014-07-02 13:27:25 +0200

    added value formatting, changes in html

--- app/result.py
+++ app/result.py
@@ -1,9 +1,20 @@
 
+format_map = {
+    'date': lambda x: x.date(),
+    'score': lambda x: "{:.3f}".format(x)
+}
+
+
+def format_value(key, value):
+    fun = format_map.get(key, lambda x: x)
+    return fun(value)
+
+
 class Result(object):
 
     def __init__(self, *initial_data, **kwargs):
         for dictionary in initial_data:
             for key in dictionary:
-                setattr(self, key, dictionary[key])
+                setattr(self, key, format_value(key, dictionary[key]))
         for key in kwargs:
-            setattr(self, key, kwargs[key])
+            setattr(self, key, format_value(key, kwargs[key]))
