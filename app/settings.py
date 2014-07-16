5491b49c8da6c6823cf786c44f52cd90c1ac7383
jankrukowski <jan.krukowski@tigerspike.com>
2014-07-16 15:00:55 +0200

    added changes in most similar inmates

--- app/settings.py
+++ app/settings.py
@@ -9,7 +9,8 @@ app.config.from_object(__name__)
 Data = namedtuple('Data', 'matrix model dictionary data_frame')
 Params = namedtuple('Params', 'user_input score_threshold')
 nltk.data.path.append(os.path.join(app.root_path, 'nltk_data/'))
-DF_COLUMNS = ['age', 'county', 'date', 'info_link', 'name', 'no', 'race', 'stm', 'stm_link', 'surename', 'county', 'ms']
+DF_COLUMNS = ['age', 'county', 'date', 'info_link', 'name', 'no', 'race', 'stm', 'stm_link', 'surename', 'county', 'ms',
+              'short_stm', 'index', 'date_str']
 
 app.config.update(dict(
     SECRET_KEY='\x00\x8ex\x06*BV\xba\x93\x9d-\x9e\xe9\x844\xaa-\xb7\x81&i\xcbJu',
